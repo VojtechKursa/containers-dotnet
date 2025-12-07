75c2b9120b5ccca5ac1eff3cff5e4a747a8df48d
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2025-12-07 13:23:08 +0100

    Add notes and script for updating and publishing images

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Dotnet containers
 
 Repository containing my custom (alpine-based) containers for .NET development.
+
+## Build
+
+```sh
+docker build --build-arg "DOTNET_VERSION={version}" --target {target} --tag vojtechkursa/dotnet:{target}-{version}-alpine --no-cache .
+```
+
+## Publish
+
+```sh
+docker push {tag}
+```
---
+++ update_and_push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Run after removing old images
+
+version=8
+subversion=0
+
+docker build --build-arg "DOTNET_VERSION=$version" --target sdk --tag vojtechkursa/dotnet:sdk-$version.$subversion-alpine .
+docker build --build-arg "DOTNET_VERSION=$version" --target dotnet --tag vojtechkursa/dotnet:dotnet-$version.$subversion-alpine .
+docker build --build-arg "DOTNET_VERSION=$version" --target aspnet --tag vojtechkursa/dotnet:aspnet-$version.$subversion-alpine .
+
+docker push vojtechkursa/dotnet:sdk-$version.$subversion-alpine
+docker push vojtechkursa/dotnet:dotnet-$version.$subversion-alpine
+docker push vojtechkursa/dotnet:aspnet-$version.$subversion-alpine
